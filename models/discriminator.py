db39d5dee3d9217c778a0e425e1a13d01a027bf4
jrahnfeld <jrahnfeld@hotmail.de>
2020-11-22 02:24:52 +0100

    adopt more dcgan like architecture

--- models/discriminator.py
+++ models/discriminator.py
@@ -7,18 +7,21 @@ class Discriminator(nn.Module):
     def __init__(self):
         super(Discriminator, self).__init__()
 
-        self.layers = [64, 64]
+        self.layers = [128, 256, 512]
 
-        conv0 = nn.Conv2d(1, self.layers[0], kernel_size=3, stride=2)
+        conv0 = nn.Conv2d(1, self.layers[0], kernel_size=6, stride=2)
         self.conv0 = nn.utils.spectral_norm(conv0)
-        conv1 = nn.Conv2d(self.layers[0], self.layers[1], kernel_size=3, stride=2)
+        conv1 = nn.Conv2d(self.layers[0], self.layers[1], kernel_size=4, stride=2, padding=1)
         self.conv1 = nn.utils.spectral_norm(conv1)
+        conv2 = nn.Conv2d(self.layers[1], self.layers[2], kernel_size=4, stride=2, padding=1)
+        self.conv2 = nn.utils.spectral_norm(conv2)
 
-        self.fc = nn.Linear(self.layers[1] * 6 * 6, 1)
+        self.fc = nn.Linear(self.layers[2] * 3 * 3, 1)
 
     def forward(self, x):
         x = F.leaky_relu(self.conv0(x), negative_slope=0.2)
         x = F.leaky_relu(self.conv1(x), negative_slope=0.2)
+        x = F.leaky_relu(self.conv2(x), negative_slope=0.2)
 
         x = torch.flatten(x, start_dim=1)
         x = torch.sigmoid(self.fc(x))
--- models/generator.py
+++ models/generator.py
@@ -8,14 +8,14 @@ class Generator(nn.Module):
         super(Generator, self).__init__()
         
         self.dim_latent = dim_latent
-        self.layers = [128, 128, 128]
+        self.layers = [512, 256, 128]
         
-        self.fc = nn.Linear(dim_latent, self.layers[0] * 7 * 7)
+        self.fc = nn.Linear(dim_latent, self.layers[0] * 3 * 3)
         self.deconv0 = nn.ConvTranspose2d(self.layers[0], self.layers[1],
                                             kernel_size=4, stride=2, padding=1)
         self.deconv1 = nn.ConvTranspose2d(self.layers[1], self.layers[2],
                                             kernel_size=4, stride=2, padding=1)
-        self.conv = nn.Conv2d(self.layers[2], 1, kernel_size=7, stride=1, padding=3)
+        self.deconv2 = nn.ConvTranspose2d(self.layers[2], 1, kernel_size=6, stride=2)
 
         self.norm0 = nn.InstanceNorm2d(self.layers[0])
         self.norm1 = nn.InstanceNorm2d(self.layers[1])
@@ -25,7 +25,7 @@ class Generator(nn.Module):
         batch_size = x.size(0)
         
         x = F.relu(self.fc(x))
-        x = x.view(batch_size, self.layers[0], 7, 7)
+        x = x.view(batch_size, self.layers[0], 3, 3)
         
         x = self.norm0(x)
         x = F.leaky_relu(self.deconv0(x), negative_slope=0.2)
@@ -34,6 +34,6 @@ class Generator(nn.Module):
         x = F.leaky_relu(self.deconv1(x), negative_slope=0.2)
 
         x = self.norm2(x)
-        img = torch.sigmoid(self.conv(x))
+        img = torch.sigmoid(self.deconv2(x))
 
         return img
