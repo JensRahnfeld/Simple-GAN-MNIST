a8f67e0e84cc20462063661374d190d02d3c055a
jrahnfeld <jrahnfeld@hotmail.de>
2021-02-14 14:45:34 +0100

    apply batchnorm to all layers except output layer

--- models/generator.py
+++ models/generator.py
@@ -2,24 +2,26 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
+from models.common_layers import _ConvTranspose2d
+
 
 class Generator(nn.Module):
     def __init__(self, dim_latent=100):
         super(Generator, self).__init__()
-        
+
         self.dim_latent = dim_latent
         self.layers = [512, 256, 128]
         
         self.fc = nn.Linear(dim_latent, self.layers[0] * 3 * 3)
-        self.deconv0 = nn.ConvTranspose2d(self.layers[0], self.layers[1],
-                                            kernel_size=4, stride=2, padding=1)
-        self.deconv1 = nn.ConvTranspose2d(self.layers[1], self.layers[2],
-                                            kernel_size=4, stride=2, padding=1)
-        self.deconv2 = nn.ConvTranspose2d(self.layers[2], 1, kernel_size=6, stride=2)
-
-        self.norm0 = nn.InstanceNorm2d(self.layers[0])
-        self.norm1 = nn.InstanceNorm2d(self.layers[1])
-        self.norm2 = nn.InstanceNorm2d(self.layers[2])
+        
+        self.deconv0 = _ConvTranspose2d(self.layers[0], self.layers[1],
+                                        kernel_size=4, stride=2, padding=1,
+                                        norm="batch", activation="leaky_relu")
+        self.deconv1 = _ConvTranspose2d(self.layers[1], self.layers[2],
+                                        kernel_size=4, stride=2, padding=1,
+                                        norm="batch", activation="leaky_relu")
+        self.deconv2 = _ConvTranspose2d(self.layers[2], 1, kernel_size=6, stride=2,
+                                        norm="none", activation="sigmoid")
 
     def forward(self, x):
         batch_size = x.size(0)
@@ -27,13 +29,8 @@ class Generator(nn.Module):
         x = F.relu(self.fc(x))
         x = x.view(batch_size, self.layers[0], 3, 3)
         
-        x = self.norm0(x)
-        x = F.leaky_relu(self.deconv0(x), negative_slope=0.2)
-        
-        x = self.norm1(x)
-        x = F.leaky_relu(self.deconv1(x), negative_slope=0.2)
-
-        x = self.norm2(x)
-        img = torch.sigmoid(self.deconv2(x))
+        x = self.deconv0(x)
+        x = self.deconv1(x)
+        img = self.deconv2(x)
 
         return img
